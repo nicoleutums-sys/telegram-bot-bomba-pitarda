e26666305cd63f63c4c617f2256c432ded94024b
nicoleutums-sys <nicoleutums@gmail.com>
2026-05-20 15:39:56 +0300

    Add files via upload
    second commit

---
+++ bot.py
@@ -0,0 +1,25 @@
+# Імпортуємо бібліотеки
+import asyncio
+from aiogram import Bot, Dispatcher
+from aiogram.types import Message
+from aiogram.filters import CommandStart
+
+# Сюди вставляємо свій токен з BotFather
+API_TOKEN = "8690071383:AAH6YZjkjBAR_CVNPk-2pTz8ttoDGf4Ocu8"
+
+# Створюємо бота і диспетчер
+bot = Bot(token=API_TOKEN)
+dp = Dispatcher()
+
+# Обробник команди /start
+@dp.message(CommandStart())
+async def start_handler(message: Message):
+    await message.answer("Привіт! Я твій перший бот!")
+
+# Головна функція для запуску бота
+async def main():
+    await dp.start_polling(bot)
+
+# Запускаємо
+if __name__ == "__main__":
+    asyncio.run(main())
